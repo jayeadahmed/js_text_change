4e20e5e86a94edf3708be2c4db53ca1661dce48b
Jayead Ahmed <124158012+jayeadahmed@users.noreply.github.com>
2023-09-02 17:00:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//text change
+function textChange(){
+document.getElementById('h2').innerHTML="Assalamu alaikum";
+}
+
+function textChange2(){
+  document.getElementById('h3').innerHTML="Assalamu alaikum";
+  }
+
+function textChange3(){
+  document.getElementById('h4').innerHTML="Assalamu alaikum";
+  }
+  function textChange4(){
+    document.getElementById('h5').innerHTML="Assalamu alaikum";
+    }
+
+    function textChange5(){
+    document.getElementById('h6').innerHTML="Assalamu alaikum";
+      }
+
+      function textChange6(){
+        document.getElementById('h7').innerHTML="Assalamu alaikum";
+          }
+
+          function textChange7(){
+            document.getElementById('h8').innerHTML="Assalamu alaikum";
+              }
+
+              function textChange8(){
+                document.getElementById('h9').innerHTML="Assalamu alaikum";
+                  }
+
+                  function textChange9(){
+                    document.getElementById('h10').innerHTML="Assalamu alaikum";
+                      }
+                      function textChange10(){
+                        document.getElementById('h11').innerHTML="Assalamu alaikum";
+                          }
